151ea72de11e00daa3e3b5dd707335a5ae1c341d
rittedevelopment <rittecfx@gmail.com>
2025-10-25 22:59:10 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 Config = {}
 
 -- Hur ofta meddelanden ska visas (i minuter)
-Config.MinInterval = 1  -- Minsta tid mellan meddelanden
-Config.MaxInterval = 1 -- Längsta tid mellan meddelanden
+Config.MinInterval = 10  -- Minsta tid mellan meddelanden
+Config.MaxInterval = 20 -- Längsta tid mellan meddelanden
 
 -- Hur länge varje meddelande ska visas (i sekunder)
 Config.DisplayTime = 6
@@ -10,18 +10,17 @@ Config.DisplayTime = 6
 -- Meddelanden
 Config.Messages = {
     "Du känner att någon stirrar på dig...",
-    "Varför är det så kallt här plötsligt?",
-    "Du hörde ett skrik i fjärran...",
-    "Något rör sig i skuggorna...",
-    "Du känner en iskall hand på din axel...",
-    "Varför luktar det plötsligt ruttet?",
-    "Du såg något i ögonvrån...",
-    "Dörren bakom dig öppnades just...",
-    "Vems andning är det du hör?",
-    "Du är inte ensam här...",
-    "Någon viskar ditt namn...",
+    "Du hör ett skrik långt bort...",
+    "Det blir plötsligt kallt...",
+    "Du känner dig iaktagen...",
+    "Du såg en skugga i ögonvrån...",
+    "Du hör att någon viskar ditt namn...",
+    "Du hör en svag röst...",
+    "Du börjar blöda näsblod",
+    "Du känner en hand på din axel men du är ensam...",
+    "Du hör hjärtslag... men inte dina egna...",
+    "Någon andas precis bakom dig...",
     "Varför följer de där fotljuden dig?",
     "Spegelbilden rörde sig utan dig...",
     "Lamporna flimrar av en anledning...",
-    "hejtest",
 }
